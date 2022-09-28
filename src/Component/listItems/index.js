424d7a1a55a38f6664e1ffa0b7b9ae3989094c08
FTspirit <90292792+FTspirit@users.noreply.github.com>
2022-09-28 13:47:30 +0700

    28 09 2022

---
+++ src/Component/listItems/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class ListItems extends React.Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+export default ListItems;
--- src/Pages/tuyendung.js
+++ src/Pages/tuyendung.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import ListItems from "../Component/listItems";
 function Tuyendung() {
   return (
     <>
+      <ListItems />
       <div>tuyen dung</div>
     </>
   );
